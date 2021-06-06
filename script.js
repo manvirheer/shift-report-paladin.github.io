1eb99fe56e86d9bfe83daa4ebf45d7dec2917684
Manvir Singh Heer <40204204+manvirheer@users.noreply.github.com>
2021-06-06 13:42:09 -0700

    Add the Briefing and Pass-On Text

--- script.js
+++ script.js
@@ -1,58 +1,74 @@
 var d = new Date();
-if((d.getDay() == 0 || d.getDay() == 6) && d.getHours() < 17){
-const inputTxtArea = document.getElementById("input");
-const outputTxtArea = document.getElementById("result");
-const btn = document.getElementById("submit");
+if ((d.getDay() == 0 || d.getDay() == 6) && d.getHours() < 17) {
+  const inputTxtArea = document.getElementById("input");
+  const outputTxtArea = document.getElementById("result");
+  const btn = document.getElementById("submit");
 
-btn.addEventListener("click", function(){
-  
-  //User's input
-  var raw = inputTxtArea.value;
+  btn.addEventListener("click", function () {
 
-  //Replacing new line to space
-  var spacedRaw = removeExtras(raw);
+    //User's input
+    var raw = inputTxtArea.value;
 
-  //Converting string to desired formatted string
-  var result = strProcessor(spacedRaw);
+    //Replacing new line to space
+    var spacedRaw = removeExtras(raw);
 
-  //Set up the output text area with the processed string
-  outputTxtArea.value = result;
+    //Converting string to desired formatted string
+    var result = strProcessor(spacedRaw);
 
-})
+    //Set up the output text area with the processed string
+    outputTxtArea.value = result;
 
-const removeExtras = (str) => {
-  result = str.replace(/(?:\r\n|\r|\n)/g, ' ');
-  result = result.replace(/\t+/gm, ' ')
-  return result;
-}
-
-const strProcessor = (str) => {
-  
-  const arr = str.split(' View Tour Session + ').reverse();
-  var formattedStr = '';
-  arr.forEach((ele) => {
-    ele = ele.replace('+ S - M232 - Colliers - Exchange Building  + ', '');
-
-    const patrolType = ele.match(/^([\w\/\-]+)/)[0] + ' Patrol';
-
-    ele = ele.replace(/^([\w\/\-]+)/, '');
-    ele = ele.replace(/\sPatrol\s/, '');
-
-    const officerName = ele.match(/\w+\s+/)[0];
-    ele = ele.replace(/\w+\s+/, '')
-    
-    //Last name in all caps
-    const officerLastName = (ele.match(/\w+/)[0]).toUpperCase();
-
-    ele = ele.replace(/\w+/, '')
-    
-    const startTime = ele.match(/\d+:\d+/)[0];
-    const endTime = ele.match(/\d+:\d+/g)[1];
-
-    console.log(startTime + ' ' + endTime + '\n')
-    formattedStr += startTime + ': S/O ' + officerName + officerLastName + ' started the ' + patrolType + '. \n' + endTime + ': S/O ' + officerName  + officerLastName + ' completed the ' + patrolType + '; all clear. \n'
   })
-  
-  return formattedStr;
-}
+
+  const removeExtras = (str) => {
+    result = str.replace(/(?:\r\n|\r|\n)/g, ' ');
+    result = result.replace(/\t+/gm, ' ')
+    return result;
+  }
+
+  let officersOnDuty = [];
+  const strProcessor = (str) => {
+
+    const arr = str.split(' View Tour Session + ').reverse();
+    let closedAbmYet = false;
+    var formattedStr = '';
+    let breifingOfficer = 'Manpreet SINGH';
+    let firstOfficerName = '';
+    let secondOfficerName = '';
+    arr.forEach((ele) => {
+      ele = ele.replace('+ S - M232 - Colliers - Exchange Building  + ', '');
+
+      const patrolType = ele.match(/^([\w\/\-]+)/)[0] + ' Patrol';
+
+      ele = ele.replace(/^([\w\/\-]+)/, '');
+      ele = ele.replace(/\sPatrol\s/, '');
+
+      const officerName = ele.match(/\w+\s+/)[0];
+      ele = ele.replace(/\w+\s+/, '')
+
+      //Last name in all caps
+      const officerLastName = (ele.match(/\w+/)[0]).toUpperCase();
+
+      ele = ele.replace(/\w+/, '')
+
+      const startTime = ele.match(/\d+:\d+/)[0];
+
+      if (closedAbmYet == false && parseInt(startTime, 10) > 8) {
+        formattedStr += "09:00 : S/Os opened the National Bank\'s Abm \n"
+        closedAbmYet = true
+      }
+
+      if (!officersOnDuty.includes(officerName + officerLastName))
+        officersOnDuty.push(officerName + officerLastName);
+
+      const endTime = ele.match(/\d+:\d+/g)[1];
+
+      formattedStr += startTime + ': S/O ' + officerName + officerLastName + ' started the ' + patrolType + '. \n' + endTime + ': S/O ' + officerName + officerLastName + ' completed the ' + patrolType + '; all clear. \n'
+    })
+    console.log(officersOnDuty)
+
+    let breifing = '07:45: S/O ' + officersOnDuty[0] + ' and ' + officersOnDuty[1] + ' are onsite and received breifing by S/O ' + breifingOfficer + '\n';
+    let initialDuties = "08:10: S/O " + officersOnDuty[0] + ' and ' + officersOnDuty[1] + ' read the pass-on and emails' + '\n';
+    return breifing + initialDuties + formattedStr;
+  }
 }
